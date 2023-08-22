3ec74d4512dde35c410ca793827cf657252583b7
yarkincaner <yarkincaner@gmail.com>
2023-08-22 15:15:21 +0300

    fix: Status indicator position on MiniCreatePost

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -18,7 +18,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
 
 	return (
 		<li className="overflow-hidden rounded-md bg-white shadow">
-			<div className="h-full px-6 py-4 flex justify-between gap-6">
+			<div className="h-full px-6 py-4 flex items-start justify-between gap-6">
 				<div className="relative">
 					<UserAvatar
 						user={{
@@ -29,23 +29,20 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
 
 					<span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
 				</div>
-
 				<Input
-					readOnly
 					onClick={() => router.push(pathName + "/submit")}
+					readOnly
 					placeholder="Create post"
 				/>
-
 				<Button
-					variant="ghost"
 					onClick={() => router.push(pathName + "/submit")}
+					variant="ghost"
 				>
 					<ImageIcon className="text-zinc-600" />
 				</Button>
-
 				<Button
-					variant="ghost"
 					onClick={() => router.push(pathName + "/submit")}
+					variant="ghost"
 				>
 					<Link2 className="text-zinc-600" />
 				</Button>
