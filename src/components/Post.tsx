e5711b06d738a607deeee026ea4e9dcbcf1120d8
yarkincaner <yarkincaner@gmail.com>
2023-08-26 14:08:14 +0300

    fix: Display username on posts instead of name

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -52,7 +52,7 @@ const Post: FC<PostProps> = ({
 								<span className="px-1">•</span>
 							</>
 						) : null}
-						<span>Posted by u/{post.author.name}</span>{" "}
+						<span>Posted by u/{post.author.username}</span>{" "}
 						{formatTimeToNow(new Date(post.createdAt))}
 					</div>
 
