65ef56996123a8a2c070a14e6e814b7442d06cb0
yarkincaner <yarkincaner@gmail.com>
2023-08-28 21:23:23 +0300

    fix: Navbar static height for Safari

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async (props: Props) => {
 	const session = await getAuthSession()
 
 	return (
-		<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
+		<div className="fixed top-0 inset-x-0 h-16 bg-zinc-100 border-b border-zinc-300 z-10 py-2">
 			<div className="container max-w-7xl h-full flex items-center justify-between gap-2">
 				{/* LOGO */}
 				<Link href={`/`} className="flex gap-2 items-center">
